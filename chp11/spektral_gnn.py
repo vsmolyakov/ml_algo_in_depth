47ca751852213f92e473001ca5513c0de2fa0ee4
Vadim Smolyakov <vsmolyakov@gmail.com>
2022-09-29 19:02:16 -0700

    chp11 code
    chp11 code

---
+++ chp11/spektral_gnn.py
@@ -0,0 +1,207 @@
+import numpy as np
+import pandas as pd
+
+import tensorflow as tf
+from tensorflow import keras
+
+import networkx as nx
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelEncoder
+from sklearn.utils import shuffle
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+
+from spektral.layers import GCNConv
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dropout, Dense
+from tensorflow.keras import Sequential
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import TensorBoard, EarlyStopping
+from tensorflow.keras.regularizers import l2
+
+import os
+from collections import Counter
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+
+np.random.seed(42)
+
+SAVE_PATH = "/content/drive/MyDrive/Colab Notebooks/data/"
+DATA_PATH = "/content/drive/MyDrive/data/cora/"
+
+column_names = ["paper_id"] + [f"term_{idx}" for idx in range(1433)] + ["subject"]
+node_df = pd.read_csv(DATA_PATH + "cora.content", sep="\t", header=None, names=column_names)
+print("Node df shape:", node_df.shape)
+
+edge_df = pd.read_csv(DATA_PATH + "cora.cites", sep="\t", header=None, names=["target", "source"])
+print("Edge df shape:", edge_df.shape)
+
+#parse node data
+nodes = node_df.iloc[:,0].tolist()
+labels = node_df.iloc[:,-1].tolist()
+X = node_df.iloc[:,1:-1].values
+
+X = np.array(X,dtype=int)
+N = X.shape[0] #the number of nodes
+F = X.shape[1] #the size of node features
+
+#parse edge data
+edge_list = [(x, y) for x, y in zip(edge_df['target'], edge_df['source'])]
+
+num_classes = len(set(labels))
+
+print('Number of nodes:', N)
+print('Number of features of each node:', F)
+print('Labels:', set(labels))
+print('Number of classes:', num_classes)
+
+def sample_data(labels, limit=20, val_num=500, test_num=1000):
+    label_counter = dict((l, 0) for l in labels)
+    train_idx = []
+
+    for i in range(len(labels)):
+        label = labels[i]
+        if label_counter[label]<limit:
+            #add the example to the training data
+            train_idx.append(i)
+            label_counter[label]+=1
+        
+        #exit the loop once we found 20 examples for each class
+        if all(count == limit for count in label_counter.values()):
+            break
+    
+    #get the indices that do not go to traning data
+    rest_idx = [x for x in range(len(labels)) if x not in train_idx]
+    #get the first val_num
+    val_idx = rest_idx[:val_num]
+    test_idx = rest_idx[val_num:(val_num+test_num)]
+    return train_idx, val_idx,test_idx
+
+
+train_idx,val_idx,test_idx = sample_data(labels)
+
+#set the mask
+train_mask = np.zeros((N,),dtype=bool)
+train_mask[train_idx] = True
+
+val_mask = np.zeros((N,),dtype=bool)
+val_mask[val_idx] = True
+
+test_mask = np.zeros((N,),dtype=bool)
+test_mask[test_idx] = True
+
+print("Training data distribution:\n{}".format(Counter([labels[i] for i in train_idx])))
+print("Validation data distribution:\n{}".format(Counter([labels[i] for i in val_idx])))
+
+def encode_label(labels):
+    label_encoder = LabelEncoder()
+    labels = label_encoder.fit_transform(labels)
+    labels = to_categorical(labels)
+    return labels, label_encoder.classes_
+
+labels_encoded, classes = encode_label(labels)
+
+#build the graph
+G = nx.Graph()
+G.add_nodes_from(nodes)
+G.add_edges_from(edge_list)
+
+#obtain the adjacency matrix (A)
+A = nx.adjacency_matrix(G)
+print('Graph info: ', nx.info(G))
+
+# Parameters
+channels = 16           # Number of channels in the first layer
+dropout = 0.5           # Dropout rate for the features
+l2_reg = 5e-4           # L2 regularization rate
+learning_rate = 1e-2    # Learning rate
+epochs = 200            # Number of training epochs
+es_patience = 10        # Patience for early stopping
+
+# Preprocessing operations
+A = GCNConv.preprocess(A).astype('f4')
+
+# Model definition
+X_in = Input(shape=(F, ))
+fltr_in = Input((N, ), sparse=True)
+
+dropout_1 = Dropout(dropout)(X_in)
+graph_conv_1 = GCNConv(channels,
+                       activation='relu',
+                       kernel_regularizer=l2(l2_reg),
+                       use_bias=False)([dropout_1, fltr_in])
+
+dropout_2 = Dropout(dropout)(graph_conv_1)
+graph_conv_2 = GCNConv(num_classes,
+                       activation='softmax',
+                       use_bias=False)([dropout_2, fltr_in])
+
+# Build model
+model = Model(inputs=[X_in, fltr_in], outputs=graph_conv_2)
+model.compile(optimizer=Adam(learning_rate=learning_rate),
+              loss='categorical_crossentropy',
+              weighted_metrics=['accuracy'])
+model.summary()
+
+# Train model
+validation_data = ([X, A], labels_encoded, val_mask)
+hist = model.fit([X, A],
+          labels_encoded,
+          sample_weight=train_mask,
+          epochs=epochs,
+          batch_size=N,
+          validation_data=validation_data,
+          shuffle=False,
+          callbacks=[
+              EarlyStopping(patience=es_patience,  restore_best_weights=True)
+          ])
+
+# Evaluate model
+X_test = X
+A_test = A
+y_test = labels_encoded
+
+y_pred = model.predict([X_test, A_test], batch_size=N)
+report = classification_report(np.argmax(y_test,axis=1), np.argmax(y_pred,axis=1), target_names=classes)
+print('GCN Classification Report: \n {}'.format(report))
+
+layer_outputs = [layer.output for layer in model.layers]
+activation_model = Model(inputs=model.input, outputs=layer_outputs)
+activations = activation_model.predict([X,A],batch_size=N)
+
+#Get t-SNE Representation
+#get the hidden layer representation after the first GCN layer
+x_tsne = TSNE(n_components=2).fit_transform(activations[3])
+
+def plot_tSNE(labels_encoded,x_tsne):
+    color_map = np.argmax(labels_encoded, axis=1)
+    plt.figure(figsize=(10,10))
+    for cl in range(num_classes):
+        indices = np.where(color_map==cl)
+        indices = indices[0]
+        plt.scatter(x_tsne[indices,0], x_tsne[indices, 1], label=cl)
+    plt.legend()
+    plt.show()
+    
+plot_tSNE(labels_encoded,x_tsne)
+
+plt.figure()
+plt.plot(hist.history['loss'], c='b', lw=2.0, label='train')
+plt.plot(hist.history['val_loss'], c='r', lw=2.0, label='val')
+plt.title('GNN model')
+plt.xlabel('Epochs')
+plt.ylabel('Cross-Entropy Loss')
+plt.legend(loc='upper right')
+plt.show()
+#plt.savefig('./figures/gnn_loss.png')
+
+plt.figure()
+plt.plot(hist.history['accuracy'], c='b', lw=2.0, label='train')
+plt.plot(hist.history['val_accuracy'], c='r', lw=2.0, label='val')
+plt.title('GNN model')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend(loc='upper left')
+plt.show()
+#plt.savefig('./figures/gnn_acc.png')
