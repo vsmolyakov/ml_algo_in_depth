bd22941ccbec08995f31e613f9864bac82d1139a
Vadim Smolyakov <vsmolyakov@gmail.com>
2022-09-29 18:43:47 -0700

    chp11 code
    chp11 code

---
+++ chp11/keras_mdn.py
@@ -0,0 +1,193 @@
+import numpy as np
+import pandas as pd
+
+import tensorflow as tf
+from tensorflow import keras
+
+from keras.models import Model
+from keras.layers import concatenate, Input
+from keras.layers import Dense, Activation, Dropout, Flatten
+from keras.layers import BatchNormalization
+
+from keras import regularizers
+from keras import backend as K
+from keras.utils import np_utils
+
+from keras.callbacks import ModelCheckpoint
+from keras.callbacks import TensorBoard
+from keras.callbacks import LearningRateScheduler 
+from keras.callbacks import EarlyStopping
+
+from sklearn.datasets import make_blobs
+from sklearn.metrics import adjusted_rand_score
+from sklearn.metrics import normalized_mutual_info_score 
+from sklearn.model_selection import train_test_split
+
+import math
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+np.random.seed(42)
+
+SAVE_PATH = "/content/drive/MyDrive/Colab Notebooks/data/"
+
+def scheduler(epoch, lr):
+    if epoch < 4:
+        return lr
+    else:
+        return lr * tf.math.exp(-0.1)
+
+def generate_data(N):
+    pi = np.array([0.2, 0.4, 0.3, 0.1])
+    mu = [[2,2], [-2,2], [-2,-2], [2,-2]]
+    std = [[0.5,0.5], [1.0,1.0], [0.5,0.5], [1.0,1.0]]
+    x = np.zeros((N,2), dtype=np.float32)
+    y = np.zeros((N,2), dtype=np.float32)
+    z = np.zeros((N,1), dtype=np.int32)
+    for n in range(N):
+        k = np.argmax(np.random.multinomial(1, pi))
+        x[n,:] = np.random.multivariate_normal(mu[k], np.diag(std[k]))
+        y[n,:] = mu[k]
+        z[n,:] = k
+    #end for
+    z = z.flatten()
+    return x, y, z, pi, mu, std
+
+def tf_normal(y, mu, sigma):
+    y_tile = K.stack([y]*num_clusters, axis=1) #[batch_size, K, D]
+    result = y_tile - mu
+    sigma_tile = K.stack([sigma]*data_dim, axis=-1) #[batch_size, K, D]
+    result = result * 1.0/(sigma_tile+1e-8)
+    result = -K.square(result)/2.0
+    oneDivSqrtTwoPI = 1.0/math.sqrt(2*math.pi)    
+    result = K.exp(result) * (1.0/(sigma_tile + 1e-8))*oneDivSqrtTwoPI
+    result = K.prod(result, axis=-1)    #[batch_size, K] iid Gaussians
+    return result
+
+def NLLLoss(y_true, y_pred):
+    out_mu = y_pred[:,:num_clusters*data_dim]
+    out_sigma = y_pred[:,num_clusters*data_dim : num_clusters*(data_dim+1)] 
+    out_pi = y_pred[:,num_clusters*(data_dim+1):]
+
+    out_mu = K.reshape(out_mu, [-1, num_clusters, data_dim])
+
+    result = tf_normal(y_true, out_mu, out_sigma)
+    result = result * out_pi
+    result = K.sum(result, axis=1, keepdims=True)
+    result = -K.log(result + 1e-8)
+    result = K.mean(result)
+    return tf.maximum(result, 0)
+
+#generate data
+X_data, y_data, z_data, pi_true, mu_true, sigma_true = generate_data(1024)
+
+data_dim = X_data.shape[1]
+num_clusters = len(mu_true)
+
+num_train = 512 
+X_train, X_test, y_train, y_test = X_data[:num_train,:], X_data[num_train:,:], y_data[:num_train,:], y_data[num_train:,:]
+z_train, z_test = z_data[:num_train], z_data[num_train:]
+
+#visualize data
+plt.figure()
+plt.scatter(X_train[:,0], X_train[:,1], c=z_train, cmap=cm.bwr)
+plt.title('training data')
+plt.show()
+#plt.savefig(SAVE_PATH + '/mdn_training_data.png')
+
+#training params
+batch_size = 128 
+num_epochs = 128 
+
+#model parameters
+hidden_size = 32
+weight_decay = 1e-4
+
+#MDN architecture
+input_data = Input(shape=(data_dim,))
+x = Dense(32, activation='relu')(input_data)
+x = Dropout(0.2)(x)
+x = BatchNormalization()(x)
+x = Dense(32, activation='relu')(x)
+x = Dropout(0.2)(x)
+x = BatchNormalization()(x)
+
+mu = Dense(num_clusters * data_dim, activation='linear')(x) #cluster means
+sigma = Dense(num_clusters, activation=K.exp)(x)            #diagonal cov
+pi = Dense(num_clusters, activation='softmax')(x)           #mixture proportions
+out = concatenate([mu, sigma, pi], axis=-1)
+
+model = Model(input_data, out)
+
+model.compile(
+  loss=NLLLoss,
+  optimizer=tf.keras.optimizers.Adam(),
+  metrics=["accuracy"]
+)
+
+model.summary()
+
+#define callbacks
+file_name = SAVE_PATH + 'mdn-weights-checkpoint.h5'
+checkpoint = ModelCheckpoint(file_name, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+reduce_lr = LearningRateScheduler(scheduler, verbose=1)
+early_stopping = EarlyStopping(monitor='val_loss', min_delta=0.01, patience=16, verbose=1)
+#tensor_board = TensorBoard(log_dir='./logs', write_graph=True)
+callbacks_list = [checkpoint, reduce_lr, early_stopping]
+
+hist = model.fit(X_train, y_train, batch_size=batch_size, epochs=num_epochs, callbacks=callbacks_list, validation_split=0.2, shuffle=True, verbose=2)
+
+y_pred = model.predict(X_test)
+
+mu_pred = y_pred[:,:num_clusters*data_dim]
+mu_pred = np.reshape(mu_pred, [-1, num_clusters, data_dim])
+sigma_pred = y_pred[:,num_clusters*data_dim : num_clusters*(data_dim+1)] 
+pi_pred = y_pred[:,num_clusters*(data_dim+1):]
+z_pred = np.argmax(pi_pred, axis=-1)
+
+rand_score = adjusted_rand_score(z_test, z_pred)
+print("adjusted rand score: ", rand_score)
+
+nmi_score = normalized_mutual_info_score(z_test, z_pred)
+print("normalized MI score: ", nmi_score)
+
+mu_pred_list = []
+sigma_pred_list = []
+for label in np.unique(z_pred):
+    z_idx = np.where(z_pred == label)[0]
+    mu_pred_lbl = np.mean(mu_pred[z_idx,label,:], axis=0)
+    mu_pred_list.append(mu_pred_lbl)
+
+    sigma_pred_lbl = np.mean(sigma_pred[z_idx,label], axis=0)
+    sigma_pred_list.append(sigma_pred_lbl)
+#end for
+
+print("true means:")
+print(np.array(mu_true))
+
+print("predicted means:")
+print(np.array(mu_pred_list))
+
+print("true sigmas:")
+print(np.array(sigma_true))
+
+print("predicted sigmas:")
+print(np.array(sigma_pred_list))
+
+#generate plots
+plt.figure()
+plt.scatter(X_test[:,0], X_test[:,1], c=z_pred, cmap=cm.bwr)
+plt.scatter(np.array(mu_pred_list)[:,0], np.array(mu_pred_list)[:,1], s=100, marker='x', lw=4.0, color='k')
+plt.title('test data')
+#plt.savefig('./figures/mdn_test_data.png')
+
+plt.figure()
+plt.plot(hist.history['loss'], c='b', lw=2.0, label='train')
+plt.plot(hist.history['val_loss'], c='r', lw=2.0, label='val')
+plt.title('Mixture Density Network')
+plt.xlabel('Epochs')
+plt.ylabel('Negative Log Likelihood Loss')
+plt.legend(loc='upper left')
+#plt.savefig('./figures/mdn_loss.png')
+
+
