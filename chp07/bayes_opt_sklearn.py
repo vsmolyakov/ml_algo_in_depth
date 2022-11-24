a8e35715be6e25b876b31ff04542a8e8531b86b1
Vadim Smolyakov <vsmolyakov@gmail.com>
2022-11-24 13:34:06 -0800

    code review

--- chp07/bayes_opt_sklearn.py
+++ chp07/bayes_opt_sklearn.py
@@ -3,7 +3,6 @@ import pandas as pd
 
 import seaborn as sns
 import matplotlib.pyplot as plt
-from matplotlib import gridspec
 
 from sklearn.datasets import make_classification
 from sklearn.model_selection import cross_val_score
@@ -41,48 +40,8 @@ def rfccv(n_estimators, max_depth):
     ).mean()
     return val
 
-def posterior(bo, x, xmin=-2, xmax=10):
-    xmin, xmax = -2, 10
-    bo.gp.fit(bo.X, bo.Y)
-    mu, sigma = bo.gp.predict(x, return_std=True)
-    return mu, sigma
-
-def plot_gp(bo, x):
-    
-    fig = plt.figure(figsize=(16, 10))
-    fig.suptitle('Gaussian Process and Utility Function After {} Steps'.format(len(bo.X)), fontdict={'size':30})
-    
-    gs = gridspec.GridSpec(2, 1, height_ratios=[3, 1]) 
-    axis = plt.subplot(gs[0])
-    acq = plt.subplot(gs[1])
-    
-    mu, sigma = posterior(bo, x)
-    axis.plot(bo.X.flatten(), bo.Y, 'D', markersize=8, label=u'Observations', color='r')
-    axis.plot(x, mu, '--', color='k', label='Prediction')
-
-    axis.fill(np.concatenate([x, x[::-1]]), 
-              np.concatenate([mu - 1.9600 * sigma, (mu + 1.9600 * sigma)[::-1]]),
-        alpha=.6, fc='c', ec='None', label='95% confidence interval')
-    
-    axis.set_xlim((0, 0.1))
-    axis.set_ylim((None, None))
-    axis.set_ylabel('f(x)', fontdict={'size':20})
-    axis.set_xlabel('x', fontdict={'size':20})
-    
-    utility = bo.util.utility(x, bo.gp, 0)
-    acq.plot(x, utility, label='Utility Function', color='purple')
-    acq.plot(x[np.argmax(utility)], np.max(utility), '*', markersize=15, 
-             label=u'Next Best Guess', markerfacecolor='gold', markeredgecolor='k', markeredgewidth=1)
-    acq.set_xlim((0, 0.1))
-    acq.set_ylim((0, np.max(utility) + 0.5))
-    acq.set_ylabel('Utility', fontdict={'size':20})
-    acq.set_xlabel('x', fontdict={'size':20})
-    
-    axis.legend()
-    acq.legend()
-    
-
 if __name__ == "__main__":
+
     gp_params = {"alpha": 1e-5}
 
     #SVM
@@ -98,25 +57,8 @@ if __name__ == "__main__":
          'max_depth': (2, 10)
         }
     )
-    rfcBO.explore({'max_depth': [2, 4, 6], 'n_estimators': [64, 128, 256]})
-
     rfcBO.maximize(init_points=4, n_iter=4, **gp_params)
 
     print('Final Results')
-    print('SVC: %f' % svcBO.res['max']['max_val'])
-    print('RFC: %f' % rfcBO.res['max']['max_val'])
-    
-    #visualize results
-    x = np.linspace(0.00001,0.1,64).reshape(-1,1)
-    plot_gp(svcBO,x)
-    plt.show()
-
-    rfc_X = map(lambda x: round(x,0), rfcBO.X[:,0])
-    rfc_Y = map(lambda x: round(x,0), rfcBO.X[:,1])
-    data = pd.DataFrame(data={'n_est':rfc_X,'max_depth':rfc_Y,'score':rfcBO.Y})
-    data = data.pivot(index='n_est', columns='max_depth', values='score')
-    sns.heatmap(data)
-    plt.show()
-    
-    
-    
+    print(svcBO.max)
+    print(rfcBO.max)
