eb4071da3f491f561fef87a772d4c01bd9a2da60
Vadim Smolyakov <vsmolyakov@gmail.com>
2022-12-29 12:24:57 -0800

    code review
    code review

--- chp11/transformer.py
+++ chp11/transformer.py
@@ -20,7 +20,7 @@ from keras.callbacks import EarlyStopping
 
 import matplotlib.pyplot as plt
 
-np.random.seed(42)
+tf.keras.utils.set_random_seed(42)
 
 SAVE_PATH = "/content/drive/MyDrive/Colab Notebooks/data/"
 
@@ -35,8 +35,8 @@ max_words = 20000 # top 20K most frequent words
 seq_len = 200  # first 200 words of each movie review
 (x_train, y_train), (x_val, y_val) = keras.datasets.imdb.load_data(num_words=max_words)
 
-x_train = sequence.pad_sequences(x_train, maxlen=seq_len)
-x_val = sequence.pad_sequences(x_val, maxlen=seq_len)
+x_train = keras.utils.pad_sequences(x_train, maxlen=seq_len)
+x_val = keras.utils.pad_sequences(x_val, maxlen=seq_len)
 
 class TransformerBlock(Layer):
     def __init__(self, embed_dim, num_heads, ff_dim, rate=0.1):
@@ -102,11 +102,11 @@ model.compile(
 
 #define callbacks
 file_name = SAVE_PATH + 'transformer-weights-checkpoint.h5'
-checkpoint = ModelCheckpoint(file_name, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+#checkpoint = ModelCheckpoint(file_name, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
 reduce_lr = LearningRateScheduler(scheduler, verbose=1)
 early_stopping = EarlyStopping(monitor='val_loss', min_delta=0.01, patience=16, verbose=1)
 #tensor_board = TensorBoard(log_dir='./logs', write_graph=True)
-callbacks_list = [checkpoint, reduce_lr, early_stopping]
+callbacks_list = [reduce_lr, early_stopping]
 
 hist = model.fit(x_train, y_train, batch_size=batch_size, epochs=num_epochs, callbacks=callbacks_list, validation_data=(x_val, y_val))
 
