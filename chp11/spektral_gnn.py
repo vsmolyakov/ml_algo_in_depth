c3b6299121385bbf7fb7dda34e1afcf209ae9593
Vadim Smolyakov <vsmolyakov@gmail.com>
2022-12-29 12:34:01 -0800

    code review
    code review

--- chp11/spektral_gnn.py
+++ chp11/spektral_gnn.py
@@ -25,7 +25,7 @@ from collections import Counter
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 
-np.random.seed(42)
+tf.keras.utils.set_random_seed(42)
 
 SAVE_PATH = "/content/drive/MyDrive/Colab Notebooks/data/"
 DATA_PATH = "/content/drive/MyDrive/data/cora/"
